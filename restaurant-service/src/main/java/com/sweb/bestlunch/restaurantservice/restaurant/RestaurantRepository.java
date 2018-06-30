f03544da6f94dae1db49790622595025c7dc47b3
Vyacheslav Stepanov <sweb1713@gmail.com>
2018-06-30 11:16:12 +0300

    refactored

--- restaurant-service/src/main/java/com/sweb/bestlunch/restaurantservice/restaurant/RestaurantRepository.java
+++ restaurant-service/src/main/java/com/sweb/bestlunch/restaurantservice/restaurant/RestaurantRepository.java
@@ -1,5 +1,6 @@
-package com.sweb.bestlunch.restaurantservice;
+package com.sweb.bestlunch.restaurantservice.restaurant;
 
+import com.sweb.bestlunch.restaurantservice.restaurant.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
